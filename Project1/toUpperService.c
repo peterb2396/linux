20f1f3bb679b522a2a5191bf830c8b8814c1675d
vboxuser <vboxuser@linux.myguest.virtualbox.org>
2023-09-06 20:57:23 -0400

    documentation

--- Project1/toUpperService.c
+++ Project1/toUpperService.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <ctype.h>
-#include "encDec.h"
+#include "encDec.h" // The project header file defines all processes required.
 
+// Main service gets ran here when calling exec. 
+// this simply prints the character returned by the required ctype function wrapper.
 int main(int argc, char *argv[])
 {
     printf("%c\n", toUpper(argv[1]));
 }
 
+// Wraps existing function into my own
+// Capitalizes and returns the passed character.
 int toUpper(char *inData)
 {
     if((int)*inData >='a' && (int)*inData <= 'z')
